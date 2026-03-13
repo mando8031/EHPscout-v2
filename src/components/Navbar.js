b8069a2a6462dd8e8154300aca73e10be390c04d
Mando8031 <157807730+mando8031@users.noreply.github.com>
2026-03-13 18:01:59 -0400

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,21 @@
-import { Link } from "react-router-dom";
+import { Link } from "react-router-dom"
 
 function Navbar(){
 
   return(
 
-    <div style={{display:"flex",gap:20,padding:20}}>
+    <div className="bg-gray-800 p-4 flex gap-6">
 
       <Link to="/">Events</Link>
+
       <Link to="/dashboard">Stats</Link>
+
       <Link to="/picklist">Picklist</Link>
 
     </div>
 
-  );
+  )
+
 }
 
-export default Navbar;
+export default Navbar
