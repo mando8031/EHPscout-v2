962d1e1917721d1955543561a6ec67a3442c352d
Mando8031 <157807730+mando8031@users.noreply.github.com>
2026-03-13 20:00:26 -0400

    Wrap App component in BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { BrowserRouter } from "react-router-dom";
+
 function App() {
+
   return (
-    <div>
-      <h1>FRC Scouting App Running</h1>
-    </div>
+    <BrowserRouter>
+      <div style={{padding:"40px"}}>
+        <h1>FRC Scouting App</h1>
+        <p>If you see this, React is working.</p>
+      </div>
+    </BrowserRouter>
   );
+
 }
 
 export default App;
