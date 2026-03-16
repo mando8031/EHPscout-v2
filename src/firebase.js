d78ad50d4eac82a4bb0fd67d43e7076adeb8797f
Mando8031 <157807730+mando8031@users.noreply.github.com>
2026-03-16 18:36:28 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,17 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBRcgAd7hMj-qfjJZ3Z6bAPAmROllToODM",
-  authDomain: "ehp5533scout-bbaed.firebaseapp.com",
-  projectId: "ehp5533scout-bbaed",
-  storageBucket: "ehp5533scout-bbaed.firebasestorage.app",
-  messagingSenderId: "11247475743",
-  appId: "1:11247475743:web:7a5081a57fea52ff38c92a"
+apiKey: "AIzaSyBRcgAd7hMj-qfjJZ3Z6bAPAmROllToODM",
+authDomain: "ehp5533scout-bbaed.firebaseapp.com",
+projectId: "ehp5533scout-bbaed",
+storageBucket: "ehp5533scout-bbaed.firebasestorage.app",
+messagingSenderId: "11247475743",
+appId: "1:11247475743:web:7a5081a57fea52ff38c92a"
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
+export const auth = getAuth(app);
