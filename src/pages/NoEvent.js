c6c738cc54438a6f829baab30d010e62cf4e6b6a
Mando8031 <157807730+mando8031@users.noreply.github.com>
2026-03-24 22:09:10 -0400

    Create NoEvent.js

---
+++ src/pages/NoEvent.js
@@ -0,0 +1,8 @@
+export default function NoEvent() {
+  return (
+    <div style={{ padding: "20px", color: "white" }}>
+      <h2>No Event Selected</h2>
+      <p>Please select an event first.</p>
+    </div>
+  );
+}
