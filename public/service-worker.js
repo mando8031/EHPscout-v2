a161629815f18906b4c7efec3ec70f484afd2c22
Mando8031 <157807730+mando8031@users.noreply.github.com>
2026-04-12 19:52:58 -0400

    Create service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,23 @@
+// public/service-worker.js
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('pwa-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/src/index.js, // Adjust with your JS build files
+        '/src/index.css', // Adjust with your CSS build files
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
