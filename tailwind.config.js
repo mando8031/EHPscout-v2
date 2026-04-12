0f10651a552f132fe547477543c133c4c7355edc
Mando8031 <157807730+mando8031@users.noreply.github.com>
2026-04-12 19:09:45 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,3 +5,11 @@ module.exports={
   },
   plugins:[]
 }
+module.exports = {
+  pwa: {
+    dest: 'public',  // The folder where the PWA assets will be placed
+    register: true,
+    skipWaiting: true,
+  },
+  // Other Next.js configurations
+};
